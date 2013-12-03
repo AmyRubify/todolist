3ce39148a89afd5e59341b42f2e9de5b2675fa16
Peter <peter@rubify.com>
2013-12-03 16:33:57 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todolist::Application.config.secret_token = '64f8681df8d7ac2901f22271b2ec70b8d326a6b3e555f4e1d217abd274e6ce5e3fa5bae3a0e3eed9fde3679ea2dfc3b5a47c2ca5fe1aa8af3091265c4f14d5b1'
